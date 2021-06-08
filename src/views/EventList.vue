8db152e123d615a06badd44834de57a36493e6d3
nadine.mueller <nadine.mueller@diva-e.com>
2021-06-08 09:08:20 +0200

    updated App.vue and optimized impotrs

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -7,8 +7,9 @@
 
 <script>
 // @ is an alias to /src
-import EventCard from '@/components/EventCard.vue';
-import EventService from '@/services/EventService.js';
+import EventService from "../services/EventService";
+import EventCard from "../components/EventCard";
+
 
 export default {
   name: 'EventList',
